31d2dafb9a7493978565aadaf871b559880f51e7
daemunozr <dae.munozr@duocuc.cl>
2026-06-18 22:59:57 -0400

    cambiado nombre de *Application.java

--- src/main/java/com/sivebo/ms_tracking/MsTrackingApplication.java
+++ src/main/java/com/sivebo/ms_tracking/MsTrackingApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsGuiasDespachoApplication {
+public class MsTrackingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsGuiasDespachoApplication.class, args);
-	}
-
+        public static void main(String[] args) {
+                SpringApplication.run(MsTrackingApplication.class, args);
+        }
 }
